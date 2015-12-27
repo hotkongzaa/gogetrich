91081ca8b8bec51d9804390d761acf0f2af5a218
hotkongzaa <hotkong.1993@gmail.com>
2015-12-27 09:59:38 +0700

    add file

--- view/trainingSchedule.php
+++ view/trainingSchedule.php
@@ -7,6 +7,7 @@ if (isset($_SESSION['expire'])) {
     }
 }
 require '../model-db-connection/config.php';
+
 ?>
 <!doctype html>
 <!--[if lt IE 7]>		<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
