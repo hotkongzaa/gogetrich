08b6768c091aa78afe27d1a5686193391cc12143
hotkongzaa <hotkong.1993@gmail.com>
2016-03-14 23:38:01 +0700

    fixed register

---
+++ view/assets/js/CourseRegis.js
@@ -0,0 +1,246 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function addMoreRegister(headerId) {
+    var firtName = $("#moreFirstName_1").val();
+    var lastName = $("#moreLastName_1").val();
+    var email = $("#moreUserEmail_1").val();
+    var phone = $("#phone_number_1").val();
+    var addressForReceipt = $("#addressForReceipt").val();
+    var addressForContact = $("#addressForContact").val();
+    var isSameAddress = "";
+    if ($("#isSameAddress").is(":checked")) {
+        isSameAddress = "true";
+    } else {
+        isSameAddress = "false";
+    }
+    if (firtName == "" && lastName == "" && phone == "" && !validatePhone(phone) && email == "" && !validateEmail(email)) {
+        showWarningNotficationDialog("<li>กรุณาระบุ ชื่อ สกุล ของผู้สมัครท่านอื่น</li><li>กรุณาระบุ เบอร์โทรศัพท์ ให้ถูกต้อง (กรุณาระบุเป็นตัวเลขเท่านั้น)</li><li>กรุณาระบุ อีเมล ให้ถูกต้อง</li>");
+        $('html,body').animate({
+            scrollTop: $("#loadMoreUser").offset().top
+        });
+    } else if (firtName == "") {
+        showWarningNotficationDialog("กรุณาระบุ ชื่อ");
+        $('html,body').animate({
+            scrollTop: $("#loadMoreUser").offset().top
+        });
+    } else if (lastName == "") {
+        showWarningNotficationDialog("กรุณาระบุ สกุล");
+        $('html,body').animate({
+            scrollTop: $("#moreFirstName_1").offset().top
+        });
+    } else if (phone == "" || !validatePhone(phone)) {
+        showWarningNotficationDialog("กรุณาระบุ เบอร์โทรศัพท์ ให้ถูกต้อง (กรุณาระบุเป็นตัวเลขเท่านั้น)");
+        $('html,body').animate({
+            scrollTop: $("#moreLastName_1").offset().top
+        });
+    } else if (email == "" || !validateEmail(email)) {
+        showWarningNotficationDialog("กรุณาระบุ อีเมล ให้ถูกต้อง");
+        $('html,body').animate({
+            scrollTop: $("#phone_number_1").offset().top
+        });
+    } else {
+        $.ajax({
+            url: "../model/com.gogetrich.function/verifyUserExistingIntmpTbleByEmail.php?email=" + email,
+            type: 'POST',
+            beforeSend: function (xhr) {
+                $(".preloader").show();
+            },
+            success: function (isDuplicate, textStatus, jqXHR) {
+                $(".preloader").fadeOut("slow");
+                if (isDuplicate == 409) {
+                    showWarningNotficationDialog("อีเมลนี้ได้ถูกใช้ในการลงทะเบียนเรียบร้อยแล้ว");
+                } else {
+                    $.ajax({
+//                        url: "../model/com.gogetrich.function/verifyEmailEnrollment.php?email=" + email + "&courseID=<?= $rowHeader['HEADER_ID'] ?>",
+                        url: "../model/com.gogetrich.function/verifyEmailEnrollment.php?email=" + email + "&courseID=" + headerId,
+                        type: 'POST',
+                        success: function (dataCheckRegistered, textStatus, jqXHR) {
+                            if (dataCheckRegistered == 200) {
+                                $.ajax({
+                                    url: "../model/com.gogetrich.function/SaveAdditionalUserToTmp.php?isSameAddress=" + isSameAddress + "&fName=" + firtName + "&lName=" + lastName + "&email=" + email + "&phone=" + phone + "&isOwner=false&addressForReceipt=" + addressForReceipt + "&addressForContact=" + addressForContact,
+                                    type: 'POST',
+                                    success: function (data, textStatus, jqXHR) {
+                                        if (data == 200) {
+                                            $("#loadMoreUser").load("moreUserTbl.php", function () {
+                                                $('html,body').animate({
+                                                    scrollTop: $("#loadMoreUser").offset().top
+                                                });
+                                            });
+                                            $("#moreFirstName_1").val("");
+                                            $("#moreLastName_1").val("");
+                                            $("#moreUserEmail_1").val("");
+                                            $("#phone_number_1").val("");
+                                            $("#addressForReceipt").val("");
+                                            $("#addressForContact").val("");
+                                            $("#isSameAddress").prop("checked", false);
+                                            $("#addressForReceipt").removeClass("blockTextArea");
+                                        } else {
+                                            showWarningNotficationDialog(data);
+                                        }
+
+                                    }
+                                });
+                                $("body, html, #scheduleFormDiv").animate({scrollTop: 0}, "fast");
+                            } else {
+                                showWarningNotficationDialog(dataCheckRegistered);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
+function validateEmail($email) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailReg.test($email);
+}
+function validatePhone($phone) {
+    var phoneReg = /[0-9 -()+]+$/;
+    return phoneReg.test($phone);
+}
+function initialPageRegisCourse(cid, fpage) {
+    //Manage login 
+    runSetDefaultValidation();
+    var form = $('.login-form');
+    var errorHandler = $('.errorHandler', form);
+    form.validate({
+        rules: {
+            username: {
+                required: true
+            },
+            password: {
+                required: true
+            }
+        },
+        submitHandler: function (form) {
+            errorHandler.hide();
+            submitLogin(form, cid, fpage);
+        },
+        invalidHandler: function (event, validator) {//display error alert on form submit
+            errorHandler.show();
+        }
+    });
+    //Loading registration table into Div
+    $.ajax({
+        url: "moreUserTbl.php",
+        type: 'POST',
+        success: function (data, textStatus, jqXHR) {
+            $("#loadMoreUser").html(data);
+        }
+    });
+    //Show login popup
+    $('#login_menu').tooltipster({
+        contentAsHTML: true,
+        content: $('<ul><li class="linkHover" onclick="logoutFromApplication()" style="list-style: none; margin-left: 10px"><div class="form-group"><i class="fa fa-sign-out"></i> Logout</div></li></ul>'),
+        touchDevices: true,
+        position: "bottom",
+        interactive: true,
+        autoClose: true,
+        trigger: "click",
+        minWidth: 150,
+        arrow: false
+    });
+}
+function submitLogin(form, cid, fPage) {
+    $.ajax({
+        url: "../model/com.gogetrich.function/LoginSubmit.php",
+        type: 'POST',
+        data: {'username': $("#usernameLogin").val(), 'password': $("#passwordLogin").val()},
+        success: function (data, textStatus, jqXHR) {
+            var resData = data.split(":");
+            if (resData[0] == 503) {
+                setTimeout(function () {
+                    showWarningNotficationDialog("ชื่อผู้ใช้ หรือ รหัสผ่านไม่ถูกต้อง");
+                }, 100);
+                $(form).trigger('reset');
+            }
+            if (resData[0] == 205) {
+                setTimeout(function () {
+                    showSuccessNotficationDialog("กรุณาเปลี่ยนรหัสผ่าน", "forceChangePassword.php?cusID=" + resData[1]);
+                }, 100);
+                $(form).trigger('reset');
+            }
+            if (resData[0] == 200) {
+                window.location = 'registrationCourse?cId=' + cid + '&fPage=' + fPage;
+                $(form).trigger('reset');
+            }
+        }
+    });
+}
+var runSetDefaultValidation = function () {
+    $.validator.setDefaults({
+        errorElement: "span", // contain the error msg in a small tag
+        errorClass: 'help-block',
+        errorPlacement: function (error, element) {// render error placement for each input type
+            if (element.attr("type") == "radio" || element.attr("type") == "checkbox") {// for chosen elements, need to insert the error after the chosen container
+                error.insertAfter($(element).closest('.form-group').children('div').children().last());
+            } else if (element.attr("name") == "card_expiry_mm" || element.attr("name") == "card_expiry_yyyy") {
+                error.appendTo($(element).closest('.form-group').children('div'));
+            } else {
+                error.insertAfter(element);
+                // for other inputs, just perform default behavior
+            }
+        },
+        ignore: ':hidden',
+        success: function (label, element) {
+            label.addClass('help-block valid');
+            // mark the current input as valid and display OK icon
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        highlight: function (element) {
+            $(element).closest('.help-block').removeClass('valid');
+            // display OK icon
+            $(element).closest('.form-group').addClass('has-error');
+            // add the Bootstrap error class to the control group
+        },
+        unhighlight: function (element) {// revert the change done by hightlight
+            $(element).closest('.form-group').removeClass('has-error');
+            // set error class to the control group
+        }
+    });
+}
+function logoutFromApplication() {
+    $.ajax({
+        url: "../model/com.gogetrich.function/Logout.php",
+        success: function (data, textStatus, jqXHR) {
+            if (data == 200) {
+                window.location.href = "main";
+            }
+        }
+    });
+}
+function deleteMoreUserTmp(tmpID) {
+    $.ajax({
+        url: "../model/com.gogetrich.function/deleteTmpAddMoreUser.php?tmpID=" + tmpID,
+        type: 'POST',
+        success: function (data2, textStatus, jqXHR) {
+            if (data2 == 200) {
+                $("#loadMoreUser").load("moreUserTbl.php");
+                $('html,body').animate({
+                    scrollTop: $("#loadMoreUser").offset().top
+                });
+            } else {
+                showWarningNotficationDialog(data2);
+            }
+        }
+    });
+}
+function resetForm() {
+    $("#moreFirstName_1").val("");
+    $("#moreLastName_1").val("");
+    $("#moreUserEmail_1").val("");
+    $("#phone_number_1").val("");
+    $("#addressForReceipt").val("");
+    $("#addressForContact").val("");
+    $("#isSameAddress").prop("checked", false);
+    $("#addressForReceipt").removeClass("blockTextArea");
+
+    $('html,body').animate({
+        scrollTop: $("#loadMoreUser").offset().top
+    });
+}
--- view/registrationCourse.php
+++ view/registrationCourse.php
@@ -30,6 +30,8 @@ if (mysql_num_rows($res) <= 0) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php include './assets/css_incl.php'; ?>
         <?php include './assets/javascript_incl.php'; ?>
+        <script src="assets/js/CourseRegis.js"></script>
+
     </head>
     <body>
         <!--[if lt IE 8]>
@@ -243,8 +245,11 @@ if (mysql_num_rows($res) <= 0) {
                                                     <label for="addressForReceipt">หากใช้ที่อยู่ที่แตกต่าง กรุณากรอกข้อมูล</label>
                                                     <textarea name="addressForReceipt" id="addressForReceipt" cols="20" style="height: 150px; padding: 4px 6px 4px 20px !important;"></textarea>
                                                 </div>
-                                                <a href="#" class="btn btn-default" onclick="addMoreRegister()">
+                                                <a href="#" class="btn btn-default" onclick="addMoreRegister('<?= $rowHeader['HEADER_ID'] ?>')">
                                                     <span class="fa fa-plus"></span> เพิ่มผู้สมัคร
+                                                </a>
+                                                <a href="#" class="btn btn-default" onclick="resetForm()">
+                                                    <span class="fa fa-eraser"></span> รีเซ็ตฟอร์ม
                                                 </a>
 
                                             </div>
@@ -350,7 +355,7 @@ if (mysql_num_rows($res) <= 0) {
             background-color: #ddd;
         }
     </style>
-    <script type="text/javascript">
+    <script>
         var state = 1;
         $(document).ready(function () {
             $("#hideRegisterForm").click(function () {
@@ -363,7 +368,7 @@ if (mysql_num_rows($res) <= 0) {
                 }
                 $("#regisMoreThan1User").toggle();
             });
-            initialPage();
+            initialPageRegisCourse('<?= $cId ?>', '<?= $fPage ?>');
             $("#isSameAddress").click(function () {
                 if ($(this).is(":checked")) {
                     $("#addressForReceipt").prop("disabled", true);
@@ -412,13 +417,17 @@ if (mysql_num_rows($res) <= 0) {
                                 url: "../model/com.gogetrich.function/CheckAndSaveEnrollment.php",
                                 type: 'POST',
                                 data: {'courseID': '<?= $cId ?>', 'paymentTerm': paymentTerm, 'seminarDiscount': seminarDiscount},
+                                beforeSend: function (xhr) {
+                                    $(".preloader").show();
+                                },
                                 success: function (data, textStatus, jqXHR) {
+                                    $(".preloader").fadeOut("slow");
                                     if (data == 200) {
                                         $.ajax({
                                             url: "../model/com.gogetrich.function/getEventDt.php?id=" + timeSchedule,
                                             type: 'POST',
                                             success: function (eventDt, textStatus, jqXHR) {
-                                                showSuccessNotficationDialog("<strong>ท่านลงทะเบียนสำเร็จแล้ว</strong><br/><strong>หลักสูตร:</strong> <?= $rowHeader['HEADER_NAME'] ?><br/><strong>ในวันที่:</strong> " + eventDt + "<br/> ขอบคุณค่ะ", "scheduleDetail?cname=<?= $cId ?>");
+                                                showSuccessNotficationDialog("<strong>ท่านลงทะเบียนสำเร็จแล้ว</strong><br/><strong>หลักสูตร:</strong> <?= $rowHeader['HEADER_NAME'] ?><br/><strong>ในวันที่:</strong> " + eventDt + "<br/> ขอบคุณค่ะ", "<?= $fPage ?>");
                                             }
                                         });
                                     } else {
@@ -483,231 +492,6 @@ if (mysql_num_rows($res) <= 0) {
                 }
             });
         });
-        function addMoreRegister() {
-            var firtName = $("#moreFirstName_1").val();
-            var lastName = $("#moreLastName_1").val();
-            var email = $("#moreUserEmail_1").val();
-            var phone = $("#phone_number_1").val();
-            var addressForReceipt = $("#addressForReceipt").val();
-            var addressForContact = $("#addressForContact").val();
-            var isSameAddress = "";
-            if ($("#isSameAddress").is(":checked")) {
-                isSameAddress = "true";
-            } else {
-                isSameAddress = "false";
-            }
-            if (firtName == "" && lastName == "" && phone == "" && !validatePhone(phone) && email == "" && !validateEmail(email)) {
-                showWarningNotficationDialog("<li>กรุณาระบุ ชื่อ สกุล ของผู้สมัครท่านอื่น</li><li>กรุณาระบุ เบอร์โทรศัพท์ ให้ถูกต้อง (กรุณาระบุเป็นตัวเลขเท่านั้น)</li><li>กรุณาระบุ อีเมล ให้ถูกต้อง</li>");
-                $('html,body').animate({
-                    scrollTop: $("#loadMoreUser").offset().top
-                });
-            } else if (firtName == "") {
-                showWarningNotficationDialog("กรุณาระบุ ชื่อ");
-                $('html,body').animate({
-                    scrollTop: $("#loadMoreUser").offset().top
-                });
-            } else if (lastName == "") {
-                showWarningNotficationDialog("กรุณาระบุ สกุล");
-                $('html,body').animate({
-                    scrollTop: $("#moreFirstName_1").offset().top
-                });
-            } else if (phone == "" || !validatePhone(phone)) {
-                showWarningNotficationDialog("กรุณาระบุ เบอร์โทรศัพท์ ให้ถูกต้อง (กรุณาระบุเป็นตัวเลขเท่านั้น)");
-                $('html,body').animate({
-                    scrollTop: $("#moreLastName_1").offset().top
-                });
-            } else if (email == "" || !validateEmail(email)) {
-                showWarningNotficationDialog("กรุณาระบุ อีเมล ให้ถูกต้อง");
-                $('html,body').animate({
-                    scrollTop: $("#phone_number_1").offset().top
-                });
-            } else {
-                $.ajax({
-                    url: "../model/com.gogetrich.function/verifyUserExistingIntmpTbleByEmail.php?email=" + email,
-                    type: 'POST',
-                    beforeSend: function (xhr) {
-                        $(".preloader").show();
-                    },
-                    success: function (isDuplicate, textStatus, jqXHR) {
-                        $(".preloader").fadeOut("slow");
-                        if (isDuplicate == 409) {
-                            showWarningNotficationDialog("อีเมลนี้ได้ถูกใช้ในการลงทะเบียนเรียบร้อยแล้ว");
-                        } else {
-                            $.ajax({
-                                url: "../model/com.gogetrich.function/verifyEmailEnrollment.php?email=" + email + "&courseID=<?= $rowHeader['HEADER_ID'] ?>",
-                                type: 'POST',
-                                success: function (dataCheckRegistered, textStatus, jqXHR) {
-                                    if (dataCheckRegistered == 200) {
-                                        $.ajax({
-                                            url: "../model/com.gogetrich.function/SaveAdditionalUserToTmp.php?isSameAddress=" + isSameAddress + "&fName=" + firtName + "&lName=" + lastName + "&email=" + email + "&phone=" + phone + "&isOwner=false&addressForReceipt=" + addressForReceipt + "&addressForContact=" + addressForContact,
-                                            type: 'POST',
-                                            success: function (data, textStatus, jqXHR) {
-                                                if (data == 200) {
-                                                    $("#loadMoreUser").load("moreUserTbl.php", function () {
-                                                        $('html,body').animate({
-                                                            scrollTop: $("#loadMoreUser").offset().top
-                                                        });
-                                                    });
-                                                    $("#moreFirstName_1").val("");
-                                                    $("#moreLastName_1").val("");
-                                                    $("#moreUserEmail_1").val("");
-                                                    $("#phone_number_1").val("");
-                                                    $("#addressForReceipt").val("");
-                                                    $("#addressForContact").val("");
-                                                    $("#isSameAddress").prop("checked", false);
-                                                    $("#addressForReceipt").removeClass("blockTextArea");
-                                                } else {
-                                                    showWarningNotficationDialog(data);
-                                                }
-
-                                            }
-                                        });
-                                        $("body, html, #scheduleFormDiv").animate({scrollTop: 0}, "fast");
-                                    } else {
-                                        showWarningNotficationDialog(dataCheckRegistered);
-                                    }
-                                }
-                            });
-                        }
-                    }
-                });
-            }
-        }
-        function validateEmail($email) {
-            var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-            return emailReg.test($email);
-        }
-        function validatePhone($phone) {
-            var phoneReg = /[0-9 -()+]+$/;
-            return phoneReg.test($phone);
-        }
-        function initialPage() {
-            //Manage login 
-            runSetDefaultValidation();
-            var form = $('.login-form');
-            var errorHandler = $('.errorHandler', form);
-            form.validate({
-                rules: {
-                    username: {
-                        required: true
-                    },
-                    password: {
-                        required: true
-                    }
-                },
-                submitHandler: function (form) {
-                    errorHandler.hide();
-                    submitLogin(form);
-                },
-                invalidHandler: function (event, validator) {//display error alert on form submit
-                    errorHandler.show();
-                }
-            });
-            //Loading registration table into Div
-            $.ajax({
-                url: "moreUserTbl.php",
-                type: 'POST',
-                success: function (data, textStatus, jqXHR) {
-                    $("#loadMoreUser").html(data);
-                }
-            });
-            //Show login popup
-            $('#login_menu').tooltipster({
-                contentAsHTML: true,
-                content: $('<ul><li class="linkHover" onclick="logoutFromApplication()" style="list-style: none; margin-left: 10px"><div class="form-group"><i class="fa fa-sign-out"></i> Logout</div></li></ul>'),
-                touchDevices: true,
-                position: "bottom",
-                interactive: true,
-                autoClose: true,
-                trigger: "click",
-                minWidth: 150,
-                arrow: false
-            });
-        }
-        function submitLogin(form) {
-            $.ajax({
-                url: "../model/com.gogetrich.function/LoginSubmit.php",
-                type: 'POST',
-                data: {'username': $("#usernameLogin").val(), 'password': $("#passwordLogin").val()},
-                success: function (data, textStatus, jqXHR) {
-                    var resData = data.split(":");
-                    if (resData[0] == 503) {
-                        setTimeout(function () {
-                            showWarningNotficationDialog("ชื่อผู้ใช้ หรือ รหัสผ่านไม่ถูกต้อง");
-                        }, 100);
-                        $(form).trigger('reset');
-                    }
-                    if (resData[0] == 205) {
-                        setTimeout(function () {
-                            showSuccessNotficationDialog("กรุณาเปลี่ยนรหัสผ่าน", "forceChangePassword.php?cusID=" + resData[1]);
-                        }, 100);
-                        $(form).trigger('reset');
-                    }
-                    if (resData[0] == 200) {
-                        window.location = 'registrationCourse?cId=<?= $cId ?>&fPage=<?= $fPage ?>';
-                        $(form).trigger('reset');
-                    }
-                }
-            });
-        }
-        var runSetDefaultValidation = function () {
-            $.validator.setDefaults({
-                errorElement: "span", // contain the error msg in a small tag
-                errorClass: 'help-block',
-                errorPlacement: function (error, element) {// render error placement for each input type
-                    if (element.attr("type") == "radio" || element.attr("type") == "checkbox") {// for chosen elements, need to insert the error after the chosen container
-                        error.insertAfter($(element).closest('.form-group').children('div').children().last());
-                    } else if (element.attr("name") == "card_expiry_mm" || element.attr("name") == "card_expiry_yyyy") {
-                        error.appendTo($(element).closest('.form-group').children('div'));
-                    } else {
-                        error.insertAfter(element);
-                        // for other inputs, just perform default behavior
-                    }
-                },
-                ignore: ':hidden',
-                success: function (label, element) {
-                    label.addClass('help-block valid');
-                    // mark the current input as valid and display OK icon
-                    $(element).closest('.form-group').removeClass('has-error');
-                },
-                highlight: function (element) {
-                    $(element).closest('.help-block').removeClass('valid');
-                    // display OK icon
-                    $(element).closest('.form-group').addClass('has-error');
-                    // add the Bootstrap error class to the control group
-                },
-                unhighlight: function (element) {// revert the change done by hightlight
-                    $(element).closest('.form-group').removeClass('has-error');
-                    // set error class to the control group
-                }
-            });
-        }
-        function logoutFromApplication() {
-            $.ajax({
-                url: "../model/com.gogetrich.function/Logout.php",
-                success: function (data, textStatus, jqXHR) {
-                    if (data == 200) {
-                        window.location.href = "main";
-                    }
-                }
-            });
-        }
-        function deleteMoreUserTmp(tmpID) {
-            $.ajax({
-                url: "../model/com.gogetrich.function/deleteTmpAddMoreUser.php?tmpID=" + tmpID,
-                type: 'POST',
-                success: function (data2, textStatus, jqXHR) {
-                    if (data2 == 200) {
-                        $("#loadMoreUser").load("moreUserTbl.php");
-                        $('html,body').animate({
-                            scrollTop: $("#loadMoreUser").offset().top
-                        });
-                    } else {
-                        showWarningNotficationDialog(data2);
-                    }
-                }
-            });
-        }
     </script>
     <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
         <div class="modal-dialog">
