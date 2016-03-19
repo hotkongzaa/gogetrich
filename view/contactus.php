4aa21e9da899ff910294532ff6d6a20c58b8366e
hotkongzaa <hotkong.1993@gmail.com>
2016-03-19 18:20:34 +0700

    fixed contact us page

--- view/contactus.php
+++ view/contactus.php
@@ -115,7 +115,7 @@ if (isset($_SESSION['expireFrontEnd'])) {
                                         <a href="aboutus">About</a>
                                     </li>
                                     <li>
-                                        <a href="#">Training/Seminar</a>
+                                        <a href="trainingSchedule">Training/Seminar</a>
                                         <ul>
                                             <li>
                                                 <a href="trainingSchedule">Training Category</a>
@@ -157,38 +157,38 @@ if (isset($_SESSION['expireFrontEnd'])) {
                                 </div>
                             </div>
                             <form class="form-contactus">
-								<fieldset>
-									<div class="row">
-										<div class="col-sm-4">
-											<div class="form-group">
-												<i class="fa fa-user"></i>
-												<input type="text" class="form-control" placeholder="Full Name">
-											</div>
-										</div>
-										<div class="col-sm-4">
-											<div class="form-group">
-												<i class="fa fa-envelope"></i>
-												<input type="email" class="form-control" placeholder="Email Address">
-											</div>
-										</div>
-										<div class="col-sm-4">
-											<div class="form-group">
-												<i class="fa fa-pencil-square-o"></i>
-												<input type="url" class="form-control" placeholder="Your Subject">
-											</div>
-										</div>
-										<div class="col-sm-12">
-											<div class="form-group">
-												<i class="fa fa-paper-plan-o"></i>
-												<textarea class="theme-textarea" placeholder="Message"></textarea>
-											</div>
-										</div>
-										<div class="col-sm-12">
-											<a href="#" class="tg-theme-btn">send</a>
-										</div>
-									</div>
-								</fieldset>
-							</form>
+                                <fieldset>
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <div class="form-group">
+                                                <i class="fa fa-user"></i>
+                                                <input type="text" id="contactName" name="contactName" class="form-control" placeholder="Full Name*">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="form-group">
+                                                <i class="fa fa-envelope"></i>
+                                                <input type="email" id="contactEmail" name="contactEmail" class="form-control" placeholder="Email Address*">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="form-group">
+                                                <i class="fa fa-pencil-square-o"></i>
+                                                <input type="text" id="contactSubject" name="contactSubject" class="form-control" placeholder="Your Subject*">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <i class="fa fa-paper-plan-o"></i>
+                                                <textarea class="theme-textarea" id="contactMessage" name="contactMessage" placeholder="Message*"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <input type="submit" style="width: 100px;" class="btn btn-lg btn-default pull-right" value="Send"/>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </form>
                         </div>
                     </div>
                 </section>
@@ -255,78 +255,14 @@ if (isset($_SESSION['expireFrontEnd'])) {
                                             </ul>
                                         </div>
                                     </div>
-                                </div>
-                                <!--div class="col-md-3 col-sm-6 col-xs-6 width-480">
-                                    <div class="tg-teammember tg-haslayout">
-                                        <figure class="tg-member-img">
-                                            <a href="#"><img src="assets/images/pinhathai_resized.jpg" alt="member name"></a>
-                                            <div class="tg-member-hover">
-                                                <ul class="tg-member-hover-icons">
-                                                    <li><a href="#"><i class="flaticon-plus79"></i></a></li>
-                                                    <li><a href="#"><i class="flaticon-share55"></i></a></li>
-                                                </ul>
-                                            </div>
-                                        </figure>
-                                        <div class="tg-member-detail tg-haslayout">
-                                            <span class="tg-member-name">อ. ปิ่นหทัย เดชสิงหรัตน์</span>
-                                            <span class="tg-member-postion">Designer at Example.com</span>
-                                            <ul class="tg-social-icon">
-                                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div-->
+                                </div>                               
                             </div>
                         </div>
                     </div>
                 </section>
                 <!--************************************
                                 Our Team End
-                *************************************-->
-                <!--************************************
-                                Testimonials Start
-                *************************************-->
-                <!--section class="tg-main-section tg-haslayout testimonial-image">
-                    <div class="container">
-                        <div class="row">
-                            <div class="tg-testimonials tg-haslayout">
-                                <div class="col-lg-10 col-md-7 col-sm-7">
-                                    <div id="tg-testi-slider" class="tg-testi-slider">
-                                        <div class="item">
-                                            <div class="tg-theme-heading">
-                                                <span>our coach</span>
-                                                <h2>วิภา เจริญกิจสุพัฒน์ <span>(อ. พี่เกียก)</span></h2>
-                                            </div>
-                                            <div class="tg-description">
-                                                <blockquote>
-                                                    <q>สามสิ่งสำคัญ ในความรู้ของการจัดการการเงิน ที่พี่อยากจะแชร์และแบ่งปันคนไทยทุกคน ได้แก่ การวางแผนงบการเงิน การอยู่อย่างพอเพียง และการใช้ชีวิตอย่างมีความสุข ...ซึ่งทุกคนทำได้  ขอเพียงรู้จักจัดการเงินอย่างฉลาด อิสระภาพทางการเงินและการใช้ชีวิต มันเป็นไปได้....และนี่แหละ คือ โชค ที่ทุกคนต้องการมี</q>
-                                                </blockquote>
-                                            </div>
-                                        </div>
-                                        <div class="item">
-                                            <div class="tg-theme-heading">
-                                                <span>our coach</span>
-                                                <h2>อ. ตู่ กามล</h2>
-                                            </div>
-                                            <div class="tg-description">
-                                                <blockquote>
-                                                    <q>คนเราทำงาน ก็เพื่อหาเงิน เพื่อความสุข หากมีปัญหาในทางการทำงานและการเงิน มันก็แค่สิ่งหนึ่งที่เกิดขึ้นในกระบวนการ การเปลี่ยนแปลง... ค่อยๆคิด ค่อยๆแก้ สิ่งสำคัญ คือ หากเราเข้าใจตนเอง เราจะสามารถนำศักยภาพ และดึงความสามารถที่มีอยู่ สร้างรายได้อย่างไม่มีข้อจำกัด ....ศักยภาพจากการเข้าใจตนเอง และความรู้อย่างถ่องแท้ในงานที่ทำ จะสร้างโชค ที่ไม่รู้จบ</q>
-                                                </blockquote>
-                                            </div>
-                                        </div>                                        
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section-->
-                <!--************************************
-                                Testimonials End
-                *************************************-->                
+                *************************************-->                               
             </main>
             <!--************************************
                             Main End
@@ -389,14 +325,6 @@ if (isset($_SESSION['expireFrontEnd'])) {
                             </div>
                             <div class="col-sm-8">
                                 <fieldset>
-                                    <!--legend>ประวัติ และคุณวุฒิ​</legend>
-                                    <li>AFPTTM – Associate Financial Planner Thailand คุณวุฒิวิชาชีพที่ปรึกษาการเงิน จากสมาคมนักวางแผนการเงินไทย</li>
-                                    <li>FChFP – Fellow Chartered Financial Practitioner คุณวุฒิทางวิชาชีพสำหรับมืออาชีพ ด้านบริการทางการเงิน จากสมาคมตัวแทนประกันชีวิตและที่ปรึกษาการเงิน</li>
-                                    <li>RFC - Registered Financial Consultants วุฒิบัตรที่ปรึกษาการเงิน จากสถาบัน IARFC สมาคมที่ปรึกษาการเงินนานาชาติ ประเทศสหรัฐอเมริกา </li>
-                                    <li>ใบอนุญาตผู้แนะนำการลงทุนด้านหลักทรัพย์ สำนักงานคณะกรรมการกำกับหลักทรัพย์และตลาดหลักทรัพย์ (กลต)</li>
-                                    <li>ใบอนุญาตตัวแทนประกันชีวิต สำนักงานคณะกรรมการกำกับและส่งเสริมการประกอบธุรกิจประกันภัย (คปภ.)</li>
-                                    <li>ปริญญาโท วิทยาศาสตร์มหาบัณฑิต (สาขาเทคโนโลยีสารสนเทศ) มหาวิทยาลัยเกษตรศาสตร์</p>
-                                    <li>ปริญญาตรี บริหารธุรกิจบัณฑิต (สาขาระบบสารสนเทศ) เกียรตินิยมอันดับ 1 สถาบันเทคโนโลยีและอาชีวศึกษา</li-->
                                     <legend>ประวัติการศึกษา</legend>
                                     <li>ปริญญาโท วิทยาศาสตร์มหาบัณฑิต (สาขาเทคโนโลยีสารสนเทศ) มหาวิทยาลัยเกษตรศาสตร์</li>
                                     <li>ปริญญาตรี บริหารธุรกิจบัณฑิต (สาขาระบบสารสนเทศ) เกียรตินิยมอันดับ 1 สถาบันเทคโนโลยีราชมงคล</li>
@@ -454,6 +382,33 @@ if (isset($_SESSION['expireFrontEnd'])) {
             });
 
             runSetDefaultValidation();
+            var formContact = $('.form-contactus');
+            var errorHandlerContact = $('.errorHandler', formContact);
+            formContact.validate({
+                rules: {
+                    contactName: {
+                        required: true
+                    },
+                    contactEmail: {
+                        required: true
+                    },
+                    contactSubject: {
+                        required: true
+                    },
+                    contactMessage: {
+                        required: true
+                    }
+                },
+                submitHandler: function (form) {
+                    errorHandlerContact.hide();
+                    sendingContactEmail(form);
+                },
+                invalidHandler: function (event, validator) {//display error alert on form submit
+                    errorHandlerContact.show();
+                }
+            });
+
+
             var form = $('.login-form');
             var errorHandler = $('.errorHandler', form);
             form.validate({
@@ -474,6 +429,11 @@ if (isset($_SESSION['expireFrontEnd'])) {
                 }
             });
         });
+        function sendingContactEmail(form) {
+            var formEle = $(form).serialize();
+            alert(formEle);
+            $(form).trigger("reset");
+        }
         function logoutFromApplication() {
             $.ajax({
                 url: "../model/com.gogetrich.function/Logout.php",
