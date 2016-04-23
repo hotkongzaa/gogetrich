753a93b19a850cb0be7f50eb70f3d5b76a676b8d
hotkongzaa <hotkong.1993@gmail.com>
2016-04-23 15:26:30 +0700

    blog update

---
+++ view/recent-post.php
@@ -0,0 +1,129 @@
+<div class="tg-widget widget-recent-post">
+    <div class="tg-widget-holder">
+        <h3>Recent Posts</h3>
+        <ul>
+            <li style="cursor: pointer" onclick="window.location.href = 'whyweplanning-blog'">
+                <p>ทำไมต้องวางแผนการเงิน</p>
+                <time datetime="2008-02-14 20:00" class="tg-post-date">23 April 2016</time>
+            </li>
+            <li style="cursor: pointer" onclick="window.location.href = 'financial-situation'">
+                <p>ตรวจสอบสถานการณ์เงิน ของเราอยู่สถานการณ์ไหน</p>
+                <time datetime="2008-02-14 20:00" class="tg-post-date">23 April 2016</time>
+            </li>
+        </ul>
+    </div>
+</div>
+<!--<div class="col-md-12 col-xs-12">
+    <div class="tg-widget widget-accordions">
+        <div class="tg-widget-holder">
+            <h3>Accordions Widget</h3>
+            <ul class="tg-haslayout accordion" id="accordion" role="tablist" aria-multiselectable="true">
+                <li class="tg-panel panel">
+                    <div class="tg-panel-heading actives" role="tab" id="headingOne">
+                        <h4 class="actives">
+                            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Our Solution</a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="tg-body">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </li>
+                <li class="tg-panel panel">
+                    <div class="tg-panel-heading" role="tab" id="headingTwo">
+                        <h4>
+                            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Our Mission</a>
+                        </h4>
+                    </div>
+                    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                        <div class="tg-body">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </li>
+                <li class="tg-panel panel">
+                    <div class="tg-panel-heading" role="tab" id="headingThree">
+                        <h4>
+                            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Our Technology</a>
+                        </h4>
+                    </div>
+                    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                        <div class="tg-body">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </li>
+                <li class="tg-panel panel">
+                    <div class="tg-panel-heading" role="tab" id="headingFour">
+                        <h4>
+                            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseThree">Our Specification</a>
+                        </h4>
+                    </div>
+                    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                        <div class="tg-body">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="col-md-12 col-xs-12">
+    <div class="tg-widget widget-flickr">
+        <div class="tg-widget-holder">
+            <h3>Flickr Widget</h3>
+            <ul>
+                <li>
+                    <div class="tg-post-img">
+                        <img src="assets/images/thumbnail/img-25.jpg" alt="image description">
+                        <div class="tg-img-hover">
+                            <a href="#" class="flaticon-plus79"></a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="tg-post-img">
+                        <img src="assets/images/thumbnail/img-26.jpg" alt="image description">
+                        <div class="tg-img-hover">
+                            <a href="#" class="flaticon-plus79"></a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="tg-post-img">
+                        <img src="assets/images/thumbnail/img-27.jpg" alt="image description">
+                        <div class="tg-img-hover">
+                            <a href="#" class="flaticon-plus79"></a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="tg-post-img">
+                        <img src="assets/images/thumbnail/img-28.jpg" alt="image description">
+                        <div class="tg-img-hover">
+                            <a href="#" class="flaticon-plus79"></a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="tg-post-img">
+                        <img src="assets/images/thumbnail/img-29.jpg" alt="image description">
+                        <div class="tg-img-hover">
+                            <a href="#" class="flaticon-plus79"></a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="tg-post-img">
+                        <img src="assets/images/thumbnail/img-30.jpg" alt="image description">
+                        <div class="tg-img-hover">
+                            <a href="#" class="flaticon-plus79"></a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>-->
