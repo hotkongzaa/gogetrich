7dd648c591bfca751762422ebfef57b3e32d8292
hotkongzaa <hotkong.1993@gmail.com>
2016-05-23 12:23:03 +0700

    fixed bug
    change href="#" to href="javascript:void(0)"

--- view/mentor_part.php
+++ view/mentor_part.php
@@ -2,23 +2,26 @@
     <div class="col-md-3 col-sm-6 col-xs-6 width-480">
         <div class="tg-teammember tg-haslayout">
             <figure class="tg-member-img">
-                <a href="#"><img src="assets/images/portfolio/wipa_image_resized.png" alt="member name"></a>
-<!--                <div class="tg-member-hover">
-                    <ul class="tg-member-hover-icons">
-                        <li><a href="#"><i class="flaticon-plus79"></i></a></li>
-                        <li><a href="#"><i class="flaticon-share55"></i></a></li>
-                    </ul>
-                </div>-->
+                <a href="javascript:void(0)">
+                    <img src="assets/images/portfolio/wipa_image_resized.png" alt="member name">
+                </a>
             </figure>
             <div class="tg-member-detail tg-haslayout">
-                <span class="tg-member-name" style="cursor: pointer" data-toggle="modal" data-target=".mentor-p_kieak">อ. วิภา เจริญกิจสุพัฒน์</span>
-                <span class="tg-member-postion">คุณวุฒิวิชาชีพที่ปรึกษาการเงิน จากสมาคมนักวางแผนการเงินไทย</span>
+                <span class="tg-member-name" style="cursor: pointer" data-toggle="modal" data-target=".mentor-p_kieak">
+                    อ. วิภา เจริญกิจสุพัฒน์
+<!--                    <span style="font-size: 12px;">
+                        CFP<sup>®</sup>, RFC, FChFP, MSIT
+                    </span>-->
+                </span>
+                <span class="tg-member-postion">
+                    คุณวุฒิวิชาชีพที่ปรึกษาการเงิน จากสมาคมนักวางแผนการเงินไทย
+                </span>
                 <ul class="tg-social-icon">
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-pinterest-p"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-tumblr"></i></a></li>
                 </ul>
             </div>
         </div>
@@ -26,25 +29,21 @@
     <div class="col-md-3 col-sm-6 col-xs-6 width-480">
         <div class="tg-teammember tg-haslayout">
             <figure class="tg-member-img">
-                <a href="#">
+                <a href="javascript:void(0)">
                     <img src="assets/images/portfolio/golf_image_resized.png" alt="member name">
                 </a>
-<!--                <div class="tg-member-hover">
-                    <ul class="tg-member-hover-icons">
-                        <li><a href="#"><i class="flaticon-plus79"></i></a></li>
-                        <li><a href="#"><i class="flaticon-share55"></i></a></li>
-                    </ul>
-                </div>-->
             </figure>
             <div class="tg-member-detail tg-haslayout">
-                <span class="tg-member-name" style="cursor: pointer" data-toggle="modal" data-target=".mentor-p_golf">อ. ณัฏฐพล พราหมณ์สำราญ</span>
+                <span class="tg-member-name" style="cursor: pointer" data-toggle="modal" data-target=".mentor-p_golf">
+                    อ. ณัฏฐพล พราหมณ์สำราญ
+                </span>
                 <span class="tg-member-postion">ผู้เชี่ยวชาญด้านการวางแผนการเงิน</span>
                 <ul class="tg-social-icon">
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-pinterest-p"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-tumblr"></i></a></li>
                 </ul>
             </div>
         </div>
